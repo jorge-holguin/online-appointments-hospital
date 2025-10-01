df542d59729c55ddf9eb8a4534fb0dd8e26ba1f6
Jorge Holguin <jorge.holguin1105@gmail.com>
2025-10-01 18:35:36 -0500

    remove console block

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,20 +16,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const allowConsoleAccess = process.env.NEXT_PUBLIC_ALLOW_CONSOLE_ACCESS === 'true'
+  // const allowConsoleAccess = process.env.NEXT_PUBLIC_ALLOW_CONSOLE_ACCESS === 'true'
   
   return (
     <html lang="en">
       <head>
         <script src="/js/passive-events.js" async></script>
         {/* Cargar el bloqueador de consola solo si ALLOW_CONSOLE_ACCESS es false */}
-        {!allowConsoleAccess && (
+        {/* {!allowConsoleAccess && (
           <script src="/js/console-blocker.js"></script>
-        )}
+        )} */}
       </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {/* Componente para bloquear la consola si está configurado */}
-        {!allowConsoleAccess && <ConsoleBlocker />}
+        {/* {!allowConsoleAccess && <ConsoleBlocker />} */}
         <DateProvider>
           {children}
         </DateProvider>
